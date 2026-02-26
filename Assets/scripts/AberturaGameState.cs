322a71e061d1b2c58ffecbe62c1c83184ba35cd2
Maurício Braga <mb@gmail.com>
2026-02-26 08:46:42 -0300

    Adicionado chaveamento de estados com a barra de espaço.

--- Assets/scripts/AberturaGameState.cs
+++ Assets/scripts/AberturaGameState.cs
@@ -6,7 +6,9 @@ public class AberturaGameState: GameBaseState
         Debug.Log("Entramos na tela inicial");
     }
     public override void updateState(GameStateManager gameState)  {
-        
+        if (Input.GetKeyDown(KeyCode.Space))  {
+            gameState.switchState(gameState.telaCreditosState);
+        }
     }
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo na tela inicial");
--- Assets/scripts/CreditosGameState.cs
+++ Assets/scripts/CreditosGameState.cs
@@ -6,7 +6,9 @@ public class CreditosGameState: GameBaseState
         Debug.Log("Entramos na tela créditos");
     }
     public override void updateState(GameStateManager gameState)  {
-        
+            if (Input.GetKeyDown(KeyCode.Space))  {
+            gameState.switchState(gameState.telaInicialState);
+        }
     }
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo da tela créditos");
