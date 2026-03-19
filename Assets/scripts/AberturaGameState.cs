8b11c530d05b301bcbe06fc056678fa502d4be5e
Maurício Braga <mb@gmail.com>
2026-03-19 09:16:57 -0300

    Temporizadas as telas de créditos e inicial.

--- Assets/scripts/AberturaGameState.cs
+++ Assets/scripts/AberturaGameState.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class AberturaGameState: GameBaseState  
 {
+    private float tempo_mudança = 0.5f;
+    private float timer;
+    private int contador;
+    private GameObject telaInicialJogo;
     public override void enterState(GameStateManager gameState)  {
         Debug.Log("Entramos na tela inicial");
+        telaInicialJogo = GameObject.Find("tela_nova_inicial_Snake_1280_1060_0");
+        telaInicialJogo.GetComponent<SpriteRenderer>().enabled = true;
+        timer = 0;
+        contador = 15;
     }
     public override void updateState(GameStateManager gameState)  {
+        if (timer < tempo_mudança)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            contador--;
+            timer = 0;
+            if (contador < 0)  
+               gameState.switchState(gameState.jogoState); 
+        }
         if (Input.GetKeyDown(KeyCode.Space))  {
             gameState.switchState(gameState.jogoState);
         }
     }
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo na tela inicial");
+        telaInicialJogo.GetComponent<SpriteRenderer>().enabled = false;
     }
 }
--- Assets/scripts/CreditosGameState.cs
+++ Assets/scripts/CreditosGameState.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class CreditosGameState: GameBaseState  
 {
+    private float tempo_mudança = 0.5f;
+    private float timer;
+    private int contador;
+    private GameObject telaCreditosJogo;
     public override void enterState(GameStateManager gameState)  {
         Debug.Log("Entramos na tela créditos");
+        telaCreditosJogo = GameObject.Find("tela_creditos_Snake_1280_1060_0");
+        telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = true;
+        timer = 0;
+        contador = 10;
     }
     public override void updateState(GameStateManager gameState)  {
+        if (timer < tempo_mudança)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            contador--;
+            timer = 0;
+            if (contador < 0)  
+                gameState.switchState(gameState.telaInicialState); 
+        }
             if (Input.GetKeyDown(KeyCode.Space))  {
             gameState.switchState(gameState.telaInicialState);
         }
     }
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo da tela créditos");
+          telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = false;
+
     }
 }
