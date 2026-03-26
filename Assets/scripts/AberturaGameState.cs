ddf01d1b9426c988aa6abc5e1ea1a16681936a28
Maurício Braga <mb@gmail.com>
2026-03-26 09:07:22 -0300

    Adicionado o SFX da cobra na tela inicial.

--- Assets/scripts/AberturaGameState.cs
+++ Assets/scripts/AberturaGameState.cs
@@ -6,12 +6,17 @@ public class AberturaGameState: GameBaseState
     private float timer;
     private int contador;
     private GameObject telaInicialJogo;
+
+    private GameObject sfxTelaInicialJogo;
+
     public override void enterState(GameStateManager gameState)  {
         Debug.Log("Entramos na tela inicial");
         telaInicialJogo = GameObject.Find("tela_nova_inicial_Snake_1280_1060_0");
         telaInicialJogo.GetComponent<SpriteRenderer>().enabled = true;
         timer = 0;
         contador = 15;
+        sfxTelaInicialJogo = GameObject.Find("snake-hissing");
+        sfxTelaInicialJogo.GetComponentInParent<AudioSource>().Play();
     }
     public override void updateState(GameStateManager gameState)  {
         if (timer < tempo_mudança)
@@ -32,5 +37,6 @@ public class AberturaGameState: GameBaseState
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo na tela inicial");
         telaInicialJogo.GetComponent<SpriteRenderer>().enabled = false;
+        sfxTelaInicialJogo.GetComponentInParent<AudioSource>().Stop();
     }
 }
